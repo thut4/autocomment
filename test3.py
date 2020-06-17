330fe3244c2f1afc17cb4a63617f2fd29326d013
thut4 <62836983+thut4@users.noreply.github.com>
2020-06-17 16:19:37 +0630

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,9 +1,10 @@
 import pyautogui                    #pip install pyautogui
 import time
-#comment=["hi","hello","helooo","nooob","aaa"] 
+#comment=["hi","hello","helooo","nooob","aaa"]  #also can be used by this line
 comment="i love you"
 time.sleep(3)
-for i in range(15):
+for i in range(15):             #you can loop whatever you like
+    #pytogui.typewrite(comment[i%7])
     pyautogui.typewrite(comment)
     pyautogui.typewrite("\n")
     time.sleep(1)
